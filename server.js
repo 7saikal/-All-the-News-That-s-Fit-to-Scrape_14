4f1a0a828dbdb6a437a661c0caaba65178e0e114
Saikal Asylbekova <saikalasylbekova@Saikals-MacBook-Pro.local>
2019-11-04 16:31:36 -0600

    updated

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapperhomework", { useNewUrlParser: true });
+var MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/scrapperhomework"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 
